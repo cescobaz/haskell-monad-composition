5634c2342bf0e56026f9fbb8c963197d3efa5243
Francesco Burelli <francesco.burelli@gmail.com>
2021-06-12 00:50:30 +0200

    solver simplified

--- src/Pascal.hs
+++ src/Pascal.hs
@@ -3,31 +3,19 @@ module Pascal where
 import           Data.Char
 import           Data.Maybe ( isJust )
 
-data Exp = Number Int | Sum Exp Exp
-
 type PascalM a = Either String a
 
 solve :: String -> PascalM Int
-solve input = compute <$> parse input
+solve = parseNumber ""
 
-parse :: String -> PascalM Exp
-parse = parseNumber
-
-parseNumber :: String -> PascalM Exp
-parseNumber = parseNumber' ""
-
-parseNumber' :: String -> String -> PascalM Exp
-parseNumber' "" "" = Left "unexpected end of input"
-parseNumber' s "" = return (Number (read s))
-parseNumber' s input@(x : xs)
-    | isDigit x = parseNumber' (s ++ [ x ]) xs
+parseNumber :: String -> String -> PascalM Int
+parseNumber "" "" = Left "unexpected end of input"
+parseNumber s "" = return (read s)
+parseNumber s input@(x : xs)
+    | isDigit x = parseNumber (s ++ [ x ]) xs
     | x == '+' = do
-        r <- parse xs
-        return (Sum l r)
+        r <- parseNumber "" xs
+        return (l + r)
     | otherwise = Left ("unexpected " ++ [ x ])
   where
-    l = Number (read s)
-
-compute :: Exp -> Int
-compute (Number n) = n
-compute (Sum l r) = compute l + compute r
+    l = read s
