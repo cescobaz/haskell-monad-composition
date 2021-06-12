d95c2380436d930f80993079758f858be2ecb322
Francesco Burelli <francesco.burelli@gmail.com>
2021-06-13 01:48:13 +0200

    main implemented

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,14 @@
 module Main where
 
+import           Pascal
+
+import           System.Environment
+
 main :: IO ()
 main = do
-    putStrLn "Hello, Haskell!"
+    expression <- head <$> getArgs
+    let (result, output) = runPascal (solve expression)
+    putStrLn output
+    case result of
+        Right r -> putStrLn ("result: " ++ show r)
+        Left err -> putStrLn ("error: " ++ err)
