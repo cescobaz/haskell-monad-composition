1b73ca7d2dd1fe7a50754b243c48b2a248181956
Francesco Burelli <francesco.burelli@gmail.com>
2021-06-13 00:00:46 +0200

    WriterT added to monad composition

--- src/Pascal.hs
+++ src/Pascal.hs
@@ -3,6 +3,7 @@ module Pascal where
 import           Control.Monad.Trans.Class      ( lift )
 import           Control.Monad.Trans.Except
 import           Control.Monad.Trans.State.Lazy
+import           Control.Monad.Trans.Writer
 
 import           Data.Char                      ( isDigit )
 import           Data.Functor.Identity
@@ -18,24 +19,30 @@ incrementComputations :: Memory -> Memory
 incrementComputations memory =
     memory { computations = ((+) 1 . computations) memory }
 
-type PascalT m a = ExceptT String (StateT Memory m) a
+type PascalT m a = ExceptT String (StateT Memory (WriterT String m)) a
 
  -- `run*T` run Transformation so compute the value and pop the inner `m` monad to outside, ready to be runned by an other run*T
-runPascalT :: (Monad m) => PascalT m a -> Memory -> m (Either String a)
-runPascalT m = evalStateT (runExceptT m)
+runPascalT :: (Monad m) => PascalT m a -> Memory -> m (Either String a, String)
+runPascalT m memory = runWriterT (evalStateT (runExceptT m) memory)
 
 -- close the top of composition with Identity monad
 type PascalM a = PascalT Identity a
 
-runPascal :: PascalM a -> Either String a
+runPascal :: PascalM a -> (Either String a, String)
 runPascal m = runIdentity (runPascalT m (Memory [] 0)) -- run the inside monad transformer (PascalT) then the external one Identity
+
+tell :: Monad m => String -> PascalT m ()
+tell = lift . lift . Control.Monad.Trans.Writer.tell . (:) ':'
 
 solve :: String -> PascalM Int
 solve input = do
+    Pascal.tell "reading memory"
     memory <- lift $ gets (lookup input . expressions)
     case memory of
-        Just result -> return result
+        Just result -> Pascal.tell ("using memory for expression " ++ input)
+            >> return result
         Nothing -> do
+            Pascal.tell ("parsing expression " ++ input)
             result <- parseNumber "" input
             lift $ modify (addExpression (input, result))
             return result
--- test/PascalTest.hs
+++ test/PascalTest.hs
@@ -11,51 +11,70 @@ test = TestList [ justNumber
                 , malformedInput
                 , concatComputations
                 , checkMemory
+                , checkWriter
                 ]
 
 justNumber :: Test
 justNumber = TestLabel "justNumber" $ TestCase $ do
-    Pascal.runPascal (Pascal.solve "0") @?= Right 0
-    Pascal.runPascal (Pascal.solve "1") @?= Right 1
-    Pascal.runPascal (Pascal.solve "4") @?= Right 4
-    Pascal.runPascal (Pascal.solve "42") @?= Right 42
-    Pascal.runPascal (Pascal.solve "420") @?= Right 420
-    Pascal.runPascal (Pascal.solve "0420") @?= Right 420
+    (fst . Pascal.runPascal) (Pascal.solve "0") @?= Right 0
+    (fst . Pascal.runPascal) (Pascal.solve "1") @?= Right 1
+    (fst . Pascal.runPascal) (Pascal.solve "4") @?= Right 4
+    (fst . Pascal.runPascal) (Pascal.solve "42") @?= Right 42
+    (fst . Pascal.runPascal) (Pascal.solve "420") @?= Right 420
+    (fst . Pascal.runPascal) (Pascal.solve "0420") @?= Right 420
 
 simpleSum :: Test
 simpleSum = TestLabel "simpleSum" $ TestCase $ do
-    Pascal.runPascal (Pascal.solve "1+1") @?= Right 2
-    Pascal.runPascal (Pascal.solve "1+0") @?= Right 1
-    Pascal.runPascal (Pascal.solve "0+1") @?= Right 1
-    Pascal.runPascal (Pascal.solve "3+5") @?= Right 8
+    (fst . Pascal.runPascal) (Pascal.solve "1+1") @?= Right 2
+    (fst . Pascal.runPascal) (Pascal.solve "1+0") @?= Right 1
+    (fst . Pascal.runPascal) (Pascal.solve "0+1") @?= Right 1
+    (fst . Pascal.runPascal) (Pascal.solve "3+5") @?= Right 8
 
 doubleSum :: Test
 doubleSum = TestLabel "doubleSum" $ TestCase $ do
-    Pascal.runPascal (Pascal.solve "1+1+1") @?= Right 3
-    Pascal.runPascal (Pascal.solve "1+0+1") @?= Right 2
-    Pascal.runPascal (Pascal.solve "0+1+3") @?= Right 4
-    Pascal.runPascal (Pascal.solve "3+5+10") @?= Right 18
-    Pascal.runPascal (Pascal.solve "3+5+10+220") @?= Right 238
+    (fst . Pascal.runPascal) (Pascal.solve "1+1+1") @?= Right 3
+    (fst . Pascal.runPascal) (Pascal.solve "1+0+1") @?= Right 2
+    (fst . Pascal.runPascal) (Pascal.solve "0+1+3") @?= Right 4
+    (fst . Pascal.runPascal) (Pascal.solve "3+5+10") @?= Right 18
+    (fst . Pascal.runPascal) (Pascal.solve "3+5+10+220") @?= Right 238
 
 malformedInput :: Test
 malformedInput = TestLabel "malformedInput" $ TestCase $ do
-    Pascal.runPascal (Pascal.solve "") @?= Left "unexpected end of input"
-    Pascal.runPascal (Pascal.solve "x") @?= Left "unexpected x"
-    Pascal.runPascal (Pascal.solve "+2") @?= Left "unexpected +"
-    Pascal.runPascal (Pascal.solve "+") @?= Left "unexpected +"
-    Pascal.runPascal (Pascal.solve "1+0+1+") @?= Left "unexpected end of input"
-    Pascal.runPascal (Pascal.solve "0+1+3+") @?= Left "unexpected end of input"
-    Pascal.runPascal (Pascal.solve "3+5+10a") @?= Left "unexpected a"
+    (fst . Pascal.runPascal) (Pascal.solve "")
+        @?= Left "unexpected end of input"
+    (fst . Pascal.runPascal) (Pascal.solve "x") @?= Left "unexpected x"
+    (fst . Pascal.runPascal) (Pascal.solve "+2") @?= Left "unexpected +"
+    (fst . Pascal.runPascal) (Pascal.solve "+") @?= Left "unexpected +"
+    (fst . Pascal.runPascal) (Pascal.solve "1+0+1+")
+        @?= Left "unexpected end of input"
+    (fst . Pascal.runPascal) (Pascal.solve "0+1+3+")
+        @?= Left "unexpected end of input"
+    (fst . Pascal.runPascal) (Pascal.solve "3+5+10a") @?= Left "unexpected a"
 
 concatComputations :: Test
 concatComputations = TestLabel "concatComputations" $ TestCase $ do
-    Pascal.runPascal (Pascal.solve "1+1+1" >> Pascal.solve "1+0+1") @?= Right 2
+    (fst . Pascal.runPascal) (Pascal.solve "1+1+1" >> Pascal.solve "1+0+1")
+        @?= Right 2
 
 checkMemory :: Test
 checkMemory = TestLabel "checkMemory" $ TestCase $ do
-    Pascal.runPascal (Pascal.solve expression' >> Pascal.solve expression''
-                      >> Pascal.solve expression'' >> Pascal.getMemory)
+    (fst . Pascal.runPascal) (Pascal.solve expression'
+                              >> Pascal.solve expression''
+                              >> Pascal.solve expression'' >> Pascal.getMemory)
         @?= Right (Pascal.Memory [ (expression'', 2), (expression', 3) ] 4)
+  where
+    expression' = "1+1+1"
+
+    expression'' = "1+0+1"
+
+checkWriter :: Test
+checkWriter = TestLabel "checkWriter" $ TestCase $ do
+    (snd . Pascal.runPascal) (Pascal.solve expression'
+                              >> Pascal.solve expression''
+                              >> Pascal.solve expression'')
+        @?= ":reading memory:parsing expression " ++ expression'
+        ++ ":reading memory:parsing expression " ++ expression''
+        ++ ":reading memory:using memory for expression " ++ expression''
   where
     expression' = "1+1+1"
 
