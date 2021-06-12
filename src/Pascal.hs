a7eb444c39c263dfc0febad0e68cb666b453f43a
Francesco Burelli <francesco.burelli@gmail.com>
2021-06-12 02:55:54 +0200

    moved to monad transformer with ExceptT

--- src/Pascal.hs
+++ src/Pascal.hs
@@ -1,20 +1,33 @@
 module Pascal where
 
-import           Data.Char ( isDigit )
+import           Control.Monad.Trans.Except
 
-type PascalM a = Either String a
+import           Data.Char                  ( isDigit )
+import           Data.Functor.Identity
+
+type PascalT m a = ExceptT String m a
+
+ -- `run*T` run Transformation so compute the value and pop the inner `m` monad to outside, ready to be runned by an other run*T
+runPascalT :: (Monad m) => PascalT m a -> m (Either String a)
+runPascalT = runExceptT
+
+-- close the top of composition with Identity monad
+type PascalM a = PascalT Identity a
+
+runPascal :: PascalM a -> Either String a
+runPascal = runIdentity . runPascalT -- run the inside monad transformer (PascalT) then the external one Identity
 
 solve :: String -> PascalM Int
 solve = parseNumber ""
 
 parseNumber :: String -> String -> PascalM Int
-parseNumber "" "" = Left "unexpected end of input"
+parseNumber "" "" = throwE "unexpected end of input"
 parseNumber s "" = return (read s)
 parseNumber s (x : xs)
     | isDigit x = parseNumber (s ++ [ x ]) xs
     | x == '+' && not (null s) = do
         r <- parseNumber "" xs
         return (l + r)
-    | otherwise = Left ("unexpected " ++ [ x ])
+    | otherwise = throwE ("unexpected " ++ [ x ])
   where
     l = read s
--- test/PascalTest.hs
+++ test/PascalTest.hs
@@ -5,38 +5,47 @@ import qualified Pascal
 import           Test.HUnit.Base hiding ( test )
 
 test :: Test
-test = TestList [ justNumber, simpleSum, doubleSum, malformedInput ]
+test = TestList [ justNumber
+                , simpleSum
+                , doubleSum
+                , malformedInput
+                , concatComputations
+                ]
 
 justNumber :: Test
 justNumber = TestLabel "justNumber" $ TestCase $ do
-    Pascal.solve "0" @?= Right 0
-    Pascal.solve "1" @?= Right 1
-    Pascal.solve "4" @?= Right 4
-    Pascal.solve "42" @?= Right 42
-    Pascal.solve "420" @?= Right 420
-    Pascal.solve "0420" @?= Right 420
+    Pascal.runPascal (Pascal.solve "0") @?= Right 0
+    Pascal.runPascal (Pascal.solve "1") @?= Right 1
+    Pascal.runPascal (Pascal.solve "4") @?= Right 4
+    Pascal.runPascal (Pascal.solve "42") @?= Right 42
+    Pascal.runPascal (Pascal.solve "420") @?= Right 420
+    Pascal.runPascal (Pascal.solve "0420") @?= Right 420
 
 simpleSum :: Test
 simpleSum = TestLabel "simpleSum" $ TestCase $ do
-    Pascal.solve "1+1" @?= Right 2
-    Pascal.solve "1+0" @?= Right 1
-    Pascal.solve "0+1" @?= Right 1
-    Pascal.solve "3+5" @?= Right 8
+    Pascal.runPascal (Pascal.solve "1+1") @?= Right 2
+    Pascal.runPascal (Pascal.solve "1+0") @?= Right 1
+    Pascal.runPascal (Pascal.solve "0+1") @?= Right 1
+    Pascal.runPascal (Pascal.solve "3+5") @?= Right 8
 
 doubleSum :: Test
 doubleSum = TestLabel "doubleSum" $ TestCase $ do
-    Pascal.solve "1+1+1" @?= Right 3
-    Pascal.solve "1+0+1" @?= Right 2
-    Pascal.solve "0+1+3" @?= Right 4
-    Pascal.solve "3+5+10" @?= Right 18
-    Pascal.solve "3+5+10+220" @?= Right 238
+    Pascal.runPascal (Pascal.solve "1+1+1") @?= Right 3
+    Pascal.runPascal (Pascal.solve "1+0+1") @?= Right 2
+    Pascal.runPascal (Pascal.solve "0+1+3") @?= Right 4
+    Pascal.runPascal (Pascal.solve "3+5+10") @?= Right 18
+    Pascal.runPascal (Pascal.solve "3+5+10+220") @?= Right 238
 
 malformedInput :: Test
 malformedInput = TestLabel "malformedInput" $ TestCase $ do
-    Pascal.solve "" @?= Left "unexpected end of input"
-    Pascal.solve "x" @?= Left "unexpected x"
-    Pascal.solve "+2" @?= Left "unexpected +"
-    Pascal.solve "+" @?= Left "unexpected +"
-    Pascal.solve "1+0+1+" @?= Left "unexpected end of input"
-    Pascal.solve "0+1+3+" @?= Left "unexpected end of input"
-    Pascal.solve "3+5+10a" @?= Left "unexpected a"
+    Pascal.runPascal (Pascal.solve "") @?= Left "unexpected end of input"
+    Pascal.runPascal (Pascal.solve "x") @?= Left "unexpected x"
+    Pascal.runPascal (Pascal.solve "+2") @?= Left "unexpected +"
+    Pascal.runPascal (Pascal.solve "+") @?= Left "unexpected +"
+    Pascal.runPascal (Pascal.solve "1+0+1+") @?= Left "unexpected end of input"
+    Pascal.runPascal (Pascal.solve "0+1+3+") @?= Left "unexpected end of input"
+    Pascal.runPascal (Pascal.solve "3+5+10a") @?= Left "unexpected a"
+
+concatComputations :: Test
+concatComputations = TestLabel "concatComputations" $ TestCase $ do
+    Pascal.runPascal (Pascal.solve "1+1+1" >> Pascal.solve "1+0+1") @?= Right 2
